1a9d8ccb0307739daf14415139a705c2b7250f2f
Ben Siggery <bensiggery28@gmail.com>
2024-01-13 15:41:18 +0000

    update init config

--- install.sh
+++ install.sh
@@ -11,11 +11,6 @@ read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
 command -v brew >/dev/null 2>&1 || \
   (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
 
-# Install Oh My Zsh
-if [ ! -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
-  (echo '💰  Installing oh-my-zsh' && yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
-fi
-
 # Install chezmoi
 command -v chezmoi >/dev/null 2>&1 || \
   (echo '👊  Installing chezmoi' && brew install chezmoi)
